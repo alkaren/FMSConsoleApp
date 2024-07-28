d7ecfb758f71351837529efa61efd457faaf5415
Alkaren Ichsan <44492038+alkaren@users.noreply.github.com>
2024-07-28 20:40:44 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,65 +5,110 @@ using Confluent.Kafka;
 using MongoDB.Driver;
 using MongoDB.Bson;
 using System.Net.NetworkInformation;
-using System.Xml.Linq;
 
 class Program
 {
+    static SerialPort serialPort;
+    //const string connectionUri = "mongodb+srv://alkarenichsan03:alka123qweasd@cluster0.z85ja0e.mongodb.net/?retryWrites=true&w=majority&connectTimeoutMS=60000&appName=Cluster0";
+
+
     static async Task Main(string[] args)
     {
-        string portName = "/dev/ttyACM0";
-        int baudRate = 9600;
+        //var settings = MongoClientSettings.FromConnectionString(connectionUri);
+        //settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+        //var client = new MongoClient(settings);
 
-        var gpsReceiver = new SerialPort(portName, baudRate);
-        gpsReceiver.Open();
+        //var result = client.GetDatabase("admin").RunCommand<BsonDocument>(new BsonDocument("ping", 1));
+        //Console.WriteLine("Pinged your deployment. You successfully connected to MongoDB!");
 
-        var config = new ProducerConfig { BootstrapServers = "20.198.250.153:9092" };
-        using var producer = new ProducerBuilder<Null, string>(config).Build();
+        string portName = "/dev/ttyACM0"; //FindSerialPort();
+        //string portName = "COM5"; //FindSerialPort();
 
-        CancellationTokenSource cts = new CancellationTokenSource();
-        Task.Run(() => ReadGpsDataAndSendToKafka(gpsReceiver, producer, cts.Token));
+        if (portName == null)
+        {
+            Console.WriteLine("GPS receiver not detected. Exiting...");
+            return;
+        }
 
-        Console.WriteLine("Press Enter to exit...");
-        Console.ReadLine();
+        serialPort = new SerialPort();
+        serialPort.PortName = portName;
+        serialPort.BaudRate = 9600;    // GPS usually communicates at 9600 baud
+        serialPort.Parity = Parity.None;
+        serialPort.DataBits = 8;
+        serialPort.StopBits = StopBits.One;
+        serialPort.Handshake = Handshake.None;
 
-        cts.Cancel();
-        gpsReceiver.Close();
+        var kafkaConfig = new ProducerConfig { BootstrapServers = "20.198.250.153:9092" };
+        using var producer = new ProducerBuilder<Null, string>(kafkaConfig).Build();
+
+        //serialPort.DataReceived += (sender, e) => DataReceivedHandler(sender, e, producer, client);
+        serialPort.DataReceived += (sender, e) => DataReceivedHandler(sender, e, producer);
+
+        try
+        {
+            serialPort.Open();
+            Console.WriteLine($"Serial port {portName} opened successfully. Waiting for GPS data...");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+            // Keep the console application running
+            while (true)
+            {
+                // Your application logic here
+                Thread.Sleep(1000); // to wait for data
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error opening serial port: " + ex.Message);
+        }
+        finally
+        {
+            serialPort.Close();
+        }
     }
 
-    static async Task ReadGpsDataAndSendToKafka(SerialPort gpsReceiver, IProducer<Null, string> producer, CancellationToken cancellationToken)
+    private static async void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e, IProducer<Null, string> producer)
     {
-        while (!cancellationToken.IsCancellationRequested)
+        SerialPort sp = (SerialPort)sender;
+        string indata = sp.ReadLine();  // Read the data from the serial port
+        var gpsData = ParseGpsData(indata);
+
+        if (gpsData != null)
         {
-            try
+            var messageData = new Dictionary<string, object>
+                {
+                    { "key", Guid.NewGuid().ToString() },
+                    { "unitid", "TH0001" },
+                    { "drivername", "Alkaren" },
+                    { "driver", "https://drive.google.com/file/d/13DzMENEjMaGIo7FJDMHrnX-QOyH5RYcI/view" },
+                    { "timestamp", DateTime.UtcNow.ToString("o") }, // ISO 8601 format
+                    { "latitude", gpsData.Latitude },
+                    { "longitude", gpsData.Longitude }
+                };
+
+
+            if (CheckInternetConnection())
             {
-                string data = gpsReceiver.ReadLine();
-                var gpsData = ParseGpsData(data);
-                if (gpsData != null)
-                {
-                    var messageData = new Dictionary<string, object>
-                    {
-                        { "key", Guid.NewGuid().ToString() },
-                        { "unitid", "TH0001" },
-                        { "drivername", "Alkaren" },
-                        { "driver", "https://drive.google.com/file/d/13DzMENEjMaGIo7FJDMHrnX-QOyH5RYcI/view" },
-                        { "timestamp", DateTime.UtcNow.ToString("dd/MM/yyyy HH:mm:ss") }, // ISO 8601 format
-                        { "latitude", gpsData.Latitude },
-                        { "longitude", gpsData.Longitude }
-                    };
-                    string message = JsonSerializer.Serialize(messageData);
-                    var result = await producer.ProduceAsync("test", new Message<Null, string> { Value = message });
-                    Console.WriteLine($"Produced message to: {result.TopicPartitionOffset}");
-                    Console.WriteLine("Sample Date: " + DateTime.UtcNow.ToString("dd/MM/yyyy HH:mm:ss"));
-                }
-                else
-                {
-                    Console.WriteLine("Nothing Sample Date: " + DateTime.UtcNow.ToString("dd/MM/yyyy HH:mm:ss"));
-                }
-                await Task.Delay(1000); // Sleep for 1 second
+                // Code to execute when there is an internet connection
+                Console.WriteLine("Connected to the internet");
+
+                string message = JsonSerializer.Serialize(messageData);
+                var result = await producer.ProduceAsync("test", new Message<Null, string> { Value = message });
+                Console.WriteLine($"Produced message to: {result.TopicPartitionOffset}");
+
+
+
+                // Get the database
+                //var database = mongoClient.GetDatabase("FleetNav");
+                //var collection = database.GetCollection<BsonDocument>("vehicle_data");
+                //BsonDocument bsonDocument = BsonDocument.Parse(message);
+                //await collection.InsertOneAsync(bsonDocument);
+                //Console.WriteLine("Document inserted successfully.");
             }
-            catch (Exception ex)
+            else
             {
-                Console.WriteLine($"Error: {ex.Message}");
+                // Code to execute when there is no internet connection
+                Console.WriteLine("No internet connection");
             }
         }
     }
@@ -95,7 +140,7 @@ class Program
                 // Convert NMEA format to decimal degrees
                 double latitude = NmeaToDecimal(latitudeNmea, latDirection);
                 double longitude = NmeaToDecimal(longitudeNmea, lonDirection);
-                //Console.WriteLine(latitude + ", " + longitude + " Date Time: " + DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss"));
+                Console.WriteLine(latitude + ", " + longitude + " Date Time: " + DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss"));
 
                 string googleMapsUrl = $"https://www.google.com/maps?q={latitude.ToString("0.000000").Replace(",", ".")},{longitude.ToString("0.000000").Replace(",", ".")}";
                 Console.WriteLine($"Google Maps URL: {googleMapsUrl}");
